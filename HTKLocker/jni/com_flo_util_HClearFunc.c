cce8ffafd1f0c7b9cac88728025b7e7064c8d3bd
cfxccn <ccnx@qq.com>
2014-12-16 16:36:14 +0800

    add HTKCLEAR

---
+++ HTKLocker/jni/com_flo_util_HClearFunc.c
@@ -0,0 +1,18 @@
+#include <jni.h>
+
+
+//main_HCopy(argc, argv);
+//	main_HInit(argc, argv);
+//	main_HRest(argc, argv);
+//main_HParse(argc, argv);
+
+//main_HVite(argc, argv);
+
+JNIEXPORT void JNICALL Java_com_flo_util_HClearFunc_HClear
+(JNIEnv * env, jclass j, jstring jconfigFilePath) {
+	Exit(0);
+}
+
+
+
+
